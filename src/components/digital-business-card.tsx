0abec2d95c7cd071f418e2a9c581805474fa7c45
HassanZohaib121 <hassan.zohaib.184@gmail.com>
2025-03-10 23:52:16 +0500

    new-theme

--- src/components/digital-business-card.tsx
+++ src/components/digital-business-card.tsx
@@ -90,7 +90,7 @@ const DigitalBusinessCard = () => {
               >
                  <div className="w-12 h-12 border-2 border-primary-foreground/30 rounded-full overflow-hidden shadow-lg bg-white flex items-center justify-center">
                   <Image
-                    src="./zuhair-smartcard/Goldman-Properties.png"
+                    src="/zuhair-smartcard/Goldman-Properties.png"
                     alt="Goldman Properties Logo"
                     className="w-10 h-10 object-contain"
                     width={50}
